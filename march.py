95b5dd2960ff9f5a298d9cb38f9b30d1a44cabec
Maksim L. <maksim.lopatin.spb@gmail.com>
2024-03-03 20:08:33 +0300

    GRU: первые результаты LSTM

--- march.py
+++ march.py
@@ -1,17 +1,81 @@
 from processing import read, process
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM, Conv1D, MaxPooling1D, Flatten, TimeDistributed, ConvLSTM2D
+from keras.utils import to_categorical
+
+def evaluate_LSTM(trainX, trainy, testX, testy):
+	verbose, epochs, batch_size = 1, 15, 64
+	n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
+	model = Sequential()
+	model.add(LSTM(100, input_shape=(n_timesteps, n_features)))
+	model.add(Dropout(0.5))
+	model.add(Dense(100, activation='relu'))
+	model.add(Dense(n_outputs, activation='softmax'))
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
+	_, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
+	return accuracy
+
+
+def evaluate_CNN_LSTM(trainX, trainy, testX, testy):
+	# define model
+	verbose, epochs, batch_size = 1, 25, 64
+	n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
+	# reshape data into time steps of sub-sequences
+	n_steps, n_length = 4, 32
+	trainX = trainX.reshape((trainX.shape[0], n_steps, n_length, n_features))
+	testX = testX.reshape((testX.shape[0], n_steps, n_length, n_features))
+	# define model
+	model = Sequential()
+	model.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu'), input_shape=(None,n_length,n_features)))
+	model.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu')))
+	model.add(TimeDistributed(Dropout(0.5)))
+	model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
+	model.add(TimeDistributed(Flatten()))
+	model.add(LSTM(10))
+	model.add(Dropout(0.5))
+	model.add(Dense(100, activation='relu'))
+	model.add(Dense(n_outputs, activation='softmax'))
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
+	_, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
+	return accuracy
+
+def evaluate_ConvLSTM2D(trainX, trainy, testX, testy):
+	# define model
+	verbose, epochs, batch_size = 1, 25, 64
+	n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
+	# reshape into subsequences (samples, time steps, rows, cols, channels)
+	n_steps, n_length = 4, 32
+	trainX = trainX.reshape((trainX.shape[0], n_steps, 1, n_length, n_features))
+	testX = testX.reshape((testX.shape[0], n_steps, 1, n_length, n_features))
+	# define model
+	model = Sequential()
+	model.add(ConvLSTM2D(filters=64, kernel_size=(1,3), activation='relu', input_shape=(n_steps, 1, n_length, n_features)))
+	model.add(Dropout(0.5))
+	model.add(Flatten())
+	model.add(Dense(100, activation='relu'))
+	model.add(Dense(n_outputs, activation='softmax'))
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
+	_, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
+	return accuracy
  
-rootdir = './csidata/1_distortion_objects/1'
 def prep_dataset(rootdir):
 	cats = read.categorize(read.listdirs(rootdir), ['bottle', 'empty'])
-	csi_b = process.to_timeseries(read.getCSI(cats['bottle'][0].path))
-	csi_e = process.to_timeseries(read.getCSI(cats['empty'][0].path))
+	csi = read.getCSI(cats['bottle'][0].path)
+	csi_b = process.to_timeseries(process.extractAm(csi.reshape(csi.shape[0], -1)))
+	csi = read.getCSI(cats['empty'][0].path)
+	csi_e = process.to_timeseries(process.extractAm(csi.reshape(csi.shape[0], -1)))
 	y_b = np.tile(np.array([0, 1]), (csi_b.shape[0], 1))
 	y_e = np.tile(np.array([1, 0]), (csi_e.shape[0], 1))
 	return np.concatenate([csi_b, csi_e]), np.concatenate([y_b, y_e])
 
-# c = csi.reshape((csi.shape[0], -1))
-# data = process.to_timeseries(csi, 1000, 500)
 
-x, y = prep_dataset(rootdir)
-print(x.shape,y)
+train_x, train_y = prep_dataset('./csidata/1_distortion_objects/1')
+test_x, test_y = prep_dataset('./csidata/1_distortion_objects/2')
+
+print('\n-----Точность LSTM:', evaluate_LSTM(train_x ,train_y, test_x, test_y))
+print('\n-----Точность CNN_LSTM:', evaluate_CNN_LSTM(train_x ,train_y, test_x, test_y))
+print('\n-----Точность ConvLSTM2D:', evaluate_ConvLSTM2D(train_x ,train_y, test_x, test_y))
--- to_csv.py
+++ to_csv.py
@@ -12,9 +12,7 @@ EXPERIMENTS = {'1': 'базовый',
                  '5': 'в другом помещении',
                  '6': 'базовый - повтор'}
 OBJECTS2 = {'empty': 'нет объекта',
-            'bottle':'термос',
-            'casserole':'кастрюля',
-            'grater': 'терка'}
+            'bottle':'термос'}
 
 def df_from_file(filepath: str, experiment: str, target: str):
   csi = read.getCSI(filepath)
@@ -35,21 +33,21 @@ for exp_name, exp_description in EXPERIMENTS.items():
 df = pd.concat(dfs)
 df.to_csv(join(OUTPUT_DIR, 'out.csv'), index=False)
 
-df_lst = []
-for i in range(int(df.shape[1] / 56)):
-  item = df[[k + i*56 for k in range(0, 56)]]
-  newDf = pd.DataFrame()
-  newDf['std_' + str(i)] = item.std(axis=1)
-  # newDf['ymax_' + str(i)] = pd.DataFrame(np.fft.fft(item.sub(item.mean(axis=1)) - 1, axis=1)).abs().max() ** 2 / df.shape[1]
-  newDf['mu42_' + str(i)] = (item ** 4).mean(axis=1) / ((item ** 2).mean(axis=1) ** 4)
-  newDf['ln_' + str(i)] = np.log(item.mean(axis=1))
-  newDf['mu32_' + str(i)] = (item ** 3).mean(axis=1) / ((item ** 2).mean(axis=1) ** (3 / 2))
-  newDf['skew_' + str(i)] = item.skew(axis=1)
-  newDf['kurt_' + str(i)] = item.kurt(axis=1)
-  df_lst.append(newDf)
+# df_lst = []
+# for i in range(int(df.shape[1] / 56)):
+#   item = df[[k + i*56 for k in range(0, 56)]]
+#   newDf = pd.DataFrame()
+#   newDf['std_' + str(i)] = item.std(axis=1)
+#   # newDf['ymax_' + str(i)] = pd.DataFrame(np.fft.fft(item.sub(item.mean(axis=1)) - 1, axis=1)).abs().max() ** 2 / df.shape[1]
+#   newDf['mu42_' + str(i)] = (item ** 4).mean(axis=1) / ((item ** 2).mean(axis=1) ** 4)
+#   newDf['ln_' + str(i)] = np.log(item.mean(axis=1))
+#   newDf['mu32_' + str(i)] = (item ** 3).mean(axis=1) / ((item ** 2).mean(axis=1) ** (3 / 2))
+#   newDf['skew_' + str(i)] = item.skew(axis=1)
+#   newDf['kurt_' + str(i)] = item.kurt(axis=1)
+#   df_lst.append(newDf)
 
-df_lst.append(df['experiment'])
-df_lst.append(df['target'])
-newDf = pd.concat(df_lst, axis=1)
-print(newDf)
-newDf.to_csv(join(OUTPUT_DIR, 'out-признаки.csv'), index=False)
+# df_lst.append(df['experiment'])
+# df_lst.append(df['target'])
+# newDf = pd.concat(df_lst, axis=1)
+# print(newDf)
+# newDf.to_csv(join(OUTPUT_DIR, 'out-признаки.csv'), index=False)
